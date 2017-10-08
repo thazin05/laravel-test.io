5528c5ad16e40ab1758552d075c1e3a62d42887a
thazin05 <ttthazinthazin@gmail.com>
2017-10-08 13:38:29 +0800

    updated files

---
+++ la-angular-restful/app/Http/Controllers/SupplierController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Supplier;
+
+class SupplierController extends Controller
+{
+    
+    public function index($id = null)
+    {
+        if($id == null)
+        {
+            return Supplier::orderBy('id','asc')->get();
+        }else
+        {
+            return $this->show($id);
+        }
+    }
+
+   
+    public function store(Request $request)
+    {
+        $supplier = new Supplier;
+        $supplier->supplierName = $request->input('supplierName');
+        $supplier->supplierEmail = $request->input('supplierEmail');
+        $supplier->supplierContact = $request->input('supplierContact');
+        $supplier->supplierPosition = $request->input('supplierPosition');
+        $supplier->save();
+        return 'Supplier record successfully created with id' . $supplier->id;
+    }
+
+    
+    public function show($id)
+    {
+        return Supplier::find($id);
+    }
+
+   
+    public function update(Request $request, $id)
+    {
+        $supplier = Supplier::find($id);
+        $supplier->supplierName = $request->input('supplierName');
+        $supplier->supplierEmail = $request->input('supplierEmail');
+        $supplier->supplierContact = $request->input('supplierContact');
+        $supplier->supplierPosition = $request->input('supplierPosition');
+        $supplier->save();
+        return 'Supplier record successfully updated with id' . $supplier->id;
+    }
+
+    
+    public function destroy($id)
+    {
+        $supplier = Supplier::find($id)->delete();
+        return 'Supplier record successfully deleted';
+    }
+}
---
+++ la-angular-restful/public/angular/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('getSupplier', [])
+	.constant('API_URL','http://127.0.0.1:8000/api/');
